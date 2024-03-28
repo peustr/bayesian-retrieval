3adf518cc6f55664f724460b562260c23326fdf5
Panagiotis Efstratiadis <pefstratiad@int5.local.snellius.surf.nl>
2024-03-28 12:10:54 +0100

    single python endpoint for training job

--- src/bret/scripts/train_dpr.py
+++ src/bret/scripts/train_dpr.py
@@ -3,10 +3,10 @@ import logging
 
 import torch
 
-from bret import BayesianBERTRetriever
+from bret import BERTRetriever, BayesianBERTRetriever
 from bret.data_loaders import make_data_loader
 from bret.file_utils import get_checkpoint_file_name, get_tokenizer_cache_file_name
-from bret.trainers import BayesianDPRTrainer
+from bret.trainers import DPRTrainer, BayesianDPRTrainer
 
 logger = logging.getLogger(__name__)
 
@@ -16,22 +16,27 @@ def main():
         format="%(asctime)s - %(levelname)s - %(name)s: %(message)s", datefmt="%Y/%m/%d %H:%M:%S", level=logging.INFO
     )
     parser = argparse.ArgumentParser()
-    parser.add_argument("--training_data_file", type=str, default="data/msmarco-train.jsonl")
-    parser.add_argument("--model_name", type=str, default="google-bert/bert-base-uncased")
-    parser.add_argument("--tokenizer_cache_dir", type=str, default="cache/tokenizer")
+    parser.add_argument("--training_data_file", default="data/msmarco-train.jsonl")
+    parser.add_argument("--model_name", default="google-bert/bert-base-uncased")
+    parser.add_argument("--method", default=None, choices=["vi"])
+    parser.add_argument("--tokenizer_cache_dir", default="cache/tokenizer")
     parser.add_argument("--num_train_qry", type=int, default=8)
     parser.add_argument("--num_train_psg", type=int, default=8)
     parser.add_argument("--num_epochs", type=int, default=4)
     parser.add_argument("--lr", type=float, default=5e-5)
     parser.add_argument("--max_qry_len", type=int, default=32)
     parser.add_argument("--max_psg_len", type=int, default=256)
-    parser.add_argument("--output_dir", type=str, default="output/")
+    parser.add_argument("--output_dir", default="output/")
     args = parser.parse_args()
 
     device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
-    logger.info("Training a Bayesian BERT retriever for DPR on MS-MARCO with variational inference.")
     logger.info("Using device: %s", device)
-    tokenizer, model = BayesianBERTRetriever.build(args.model_name, device=device)
+    if args.method == "vi":
+        logger.info("Training a Bayesian BERT retriever for DPR on MS-MARCO with variational inference.")
+        tokenizer, model = BayesianBERTRetriever.build(args.model_name, device=device)
+    else:
+        logger.info("Training a BERT retriever for DPR on MS-MARCO.")
+        tokenizer, model = BERTRetriever.build(args.model_name, device=device)
     train_dl = make_data_loader(
         tokenizer,
         args.training_data_file,
@@ -42,14 +47,25 @@ def main():
         split="train",
         tokenizer_cache_file_name=get_tokenizer_cache_file_name(args.tokenizer_cache_dir, args.model_name),
     )
-    BayesianDPRTrainer.train(
-        model,
-        train_dl,
-        device,
-        lr=args.lr,
-        num_epochs=args.num_epochs,
-        ckpt_file_name=get_checkpoint_file_name(args.output_dir, args.model_name, method="vi"),
-    )
+    ckpt_file_name = get_checkpoint_file_name(args.output_dir, args.model_name, method=args.method)
+    if args.method == "vi":
+        BayesianDPRTrainer.train(
+            model,
+            train_dl,
+            device,
+            lr=args.lr,
+            num_epochs=args.num_epochs,
+            ckpt_file_name=ckpt_file_name,
+        )
+    else:
+        DPRTrainer.train(
+            model,
+            train_dl,
+            device,
+            lr=args.lr,
+            num_epochs=args.num_epochs,
+            ckpt_file_name=ckpt_file_name,
+        )
     logger.info("Training finished after %d epochs.", args.num_epochs)
 
 
